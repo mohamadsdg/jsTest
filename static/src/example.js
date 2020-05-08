0ddb4dacc998a7cc08f61ade44d6e76dc091d1e8
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-05-08 22:39:52 +0430

    adding eslint

---
+++ static/src/example.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const name = "Freddy";
+typeof name === "strng";
+
+if (!"serviceWorker" in navigator) {
+  // you have an old browser :-(
+}
+const greeting = "hello";
+console.log(`${greeting} world!`)
+[(1, 2, 3)].forEach((x) => console.log(x));
